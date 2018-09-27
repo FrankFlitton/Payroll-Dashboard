de7a9af35d8e6f6c703ff6354f535b30dee0d508
Frank Flitton <fflitton@gmail.com>
2018-09-26 22:55:55 -0400

    First stab at CSV reader

--- backend/payroll/report/views.py
+++ backend/payroll/report/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.views import APIView
+
+import csv
 
 # Create your views here.
+@api_view(['POST'])
+@parser_classes((MultiPartParser,))
+def csv_reader_view(request, format=None):
+    f = open(
+        request.FILES["file"],
+        'rb'
+    )
+    reader = csv.reader(f)
+    for row in reader:
+        print row
+    f.close()
+
+    return Response(
+        status=204,
+        {'received data': request.data}
+    )
