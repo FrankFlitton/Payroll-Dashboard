bf6fcb82f70c4af815820254603bc82d03fcece5
Frank Flitton <fflitton@gmail.com>
2018-09-27 21:50:18 -0400

    Prepopulate data

--- backend/payroll/report/views.py
+++ backend/payroll/report/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from payroll.report.serializers import UserSerializer, GroupSerializer, PayrollFileSerializer
+from .models import Employee, JobGroup, TimeReport
 
 import pdb
 import io
@@ -32,16 +33,53 @@ class FileView(APIView):
     def post(self, request, *args, **kwargs):
         file_serializer = PayrollFileSerializer(data=request.data)
         if file_serializer.is_valid():
-            print("file_serializer_____")
-            print(file_serializer)
 
+            # obtain CSV file
             csv_file = request.FILES['file']
-
             csv_file.seek(0)
             reader = csv.DictReader(io.StringIO(csv_file.read().decode('utf-8')))
 
-            for row in reader:
-                print(row)
+            print('**********************')
+            rows = list(reader)
+            reader_len = len(rows) - 1
+
+            # Create Time Report
+            time_report = rows[reader_len].get('hours worked') # 2nd col
+            if len(time_report) > 0:
+                try:
+                    time_report_obj = TimeReport.objects.get(id=time_report)
+                except TimeReport.DoesNotExist:
+                    time_report_obj = TimeReport(id=time_report)
+                    time_report_obj.save()
+
+            # Read each row
+            row_counter = 0
+            for row in rows:
+
+                # Ignore last row
+                row_counter = row_counter + 1
+
+                if row_counter > reader_len:
+
+                    # Set up variables
+                    employee_id = row.get('employee id')
+                    job_group = row.get('job group')
+                    job_group = JobGroup.objects.get(id=job_group)
+
+                    # Update or create
+                    if len(employee_id) > 0:
+                        try:
+                            employee_obj = Employee.objects.get(id=employee_id)
+                        except Employee.DoesNotExist:
+                            employee_obj = Employee(id=employee_id, employee_id=employee_id)
+                            employee_obj.save()
+
+                    # if len(job_group) > 0:
+                    #     try:
+                    #         obj = JobGroup.objects.get(id=employee_id)
+                    #     except JobGroup.DoesNotExist:
+                    #         obj = JobGroup(id=employee_id, employee_id=employee_id)
+                    #         obj.save()
 
             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         else:
