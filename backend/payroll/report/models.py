37c2b1c023abcd87955265ba2aa28a564a5467fb
Frank Flitton <fflitton@gmail.com>
2018-09-28 20:56:33 -0400

    Annotate Models

--- backend/payroll/report/models.py
+++ backend/payroll/report/models.py
@@ -1,27 +1,29 @@
 from django.db import models
 
+# Models to support the provided
 
-# Models to support the spec
 class Employee(models.Model):
-    id = models.CharField(max_length=64, blank=False, primary_key=True)
-    employee_id = models.CharField(max_length=64, blank=True)
+    id = models.CharField(max_length=64, blank=False, unique=True, primary_key=True) # Assuming any user entered string
 
 class JobGroup(models.Model):
-    id = models.CharField(max_length=128, blank=False, unique=True, primary_key=True)
-    compensation = models.DecimalField(max_digits=12, decimal_places=2, blank=False)
+    id = models.CharField(max_length=128, blank=False, unique=True, primary_key=True) # Assuming any user entered string
+    compensation = models.DecimalField(max_digits=12, decimal_places=2, blank=False) # Monetary values, save max value of 9,999,999,999.99
 
 class TimeReport(models.Model):
-    id = models.CharField(max_length=128, blank=False, unique=True, primary_key=True)
+    id = models.CharField(max_length=128, blank=False, unique=True, primary_key=True) # Assuming any user entered string
 
 class TimeSheet(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    pay_date = models.DateTimeField(blank=False)
-    pay_period = models.CharField(max_length=64, blank=True, null=True)
+    id = models.BigAutoField(primary_key=True) # internal ID, all time sheets are unique
+    pay_date = models.DateTimeField(blank=False) # date field
+    pay_period = models.CharField(max_length=64, blank=True, null=True) # generated on csv POST. Stored as plain text.
     hours_worked = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     job_group = models.ForeignKey(JobGroup, on_delete=models.PROTECT, null=True)
     employee = models.ForeignKey(Employee, on_delete=models.PROTECT, null=True)
     report = models.ForeignKey(TimeReport, on_delete=models.PROTECT, null=True)
 
+# Django needed this to allow for file upload
+# Data not used in application
+# Could be useful to retain down the road
 class PayrollFile(models.Model):
     id = models.AutoField(primary_key=True)
     file = models.FileField(blank=False, null=False)
