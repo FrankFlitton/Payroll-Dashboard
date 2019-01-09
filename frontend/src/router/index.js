197b6eb693a2ee39929b64f4f70b265fa6a3eaff
Frank Flitton <fflitton@gmail.com>
2019-01-09 10:26:00 -0500

    Remove hello world from route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import PayrollDashboard from '@/components/PayrollDashboard'
 
 Vue.use(Router)
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'PayrollDashboard',
       component: PayrollDashboard
-    },
-    {
-      path: '/HelloWorld',
-      name: 'HelloWorld',
-      component: HelloWorld
     }
   ]
 })
